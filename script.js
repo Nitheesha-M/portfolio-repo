0ec790b46ccd685efc6b5524a48edf4cae67e9c0
Nitheesha-M <macherlanitheesha608@gmail.com>
2025-10-07 23:24:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value.trim();
+
+  if (task === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  let ul = document.getElementById("taskList");
+  let li = document.createElement("li");
+  li.textContent = task;
+
+  // delete on click
+  li.onclick = function() {
+    ul.removeChild(li);
+  };
+
+  ul.appendChild(li);
+  input.value = "";
+}
